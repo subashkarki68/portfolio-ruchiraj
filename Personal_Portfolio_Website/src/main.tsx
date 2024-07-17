74d07e9eb3d7761759dfd37923eee710b65386d1
Ruchi Raj Karki <ruchirajkarki@gmail.com>
2024-07-17 09:31:06 +0545

    "Deleted globals.css, added @gsap/react dependency, removed vite.svg, and updated Home.tsx and main.tsx files"

--- Personal_Portfolio_Website/src/main.tsx
+++ Personal_Portfolio_Website/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "../app/globals.css";
 import Home from "./components/Home.tsx";
 import ErrorPage from "./error-page.tsx";
+import "./globals.css";
 import HomeLayout from "./layouts/HomeLayout.tsx";
 
 const homeChildren = [{ index: true, element: <Home /> }];
