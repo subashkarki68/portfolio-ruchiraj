103c9370f377da154d12f3d1404f61b0f02f8fcd
Ruchi Raj Karki <ruchirajkarki@gmail.com>
2024-10-03 17:59:53 +0545

    Update FancyButton.tsx

--- Personal_Portfolio_Website/src/components/FancyButton.tsx
+++ Personal_Portfolio_Website/src/components/FancyButton.tsx
@@ -1,4 +1,4 @@
-import resumePDF from "@/assets/pdf/resume_ruchi_raj_karki-sep.pdf";
+import resumePDF from "@/assets/pdf/resume_ruchi_raj_karki-octo.pdf";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
