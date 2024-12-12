f06871e2e7930edffe82dbc33f7e17f3c0525a4a
Ruchi Raj Karki <ruchirajkarki@gmail.com>
2024-12-12 17:54:43 +0545

    fix padding

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -22,9 +22,9 @@ export default function ErrorPage() {
       <Link
         to={"/"}
         onClick={() => navigate(-1)}
-        className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+        className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 bg-white text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
       >
-        <div className="z-10">
+        <div className="px-4 py-2 z-10 flex items-center justify-center">
           Return to the website
         </div>
       </Link>
